ddcfcf01c60ce10d801e71034300a113f13b5dc2
Pakiica <101930798+pakiica@users.noreply.github.com>
2022-03-19 13:59:09 +0100

    Add files via upload

---
+++ kontakt.php
@@ -0,0 +1,217 @@
+
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Ivanovic co - Podgorica</title>
+  <meta name="description" content="." />
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <style>
+      body{
+          background-color: yellow;
+        }
+      </style>
+</head>
+
+<body style="font-family: Raleway, courier, serif;">
+<div class="header"> 
+</div>
+
+<div class="topnav">
+<a href="nikola.php">Home</a>
+     <a href="kontakt.php">Kontakt</a>
+    <a href="omeni.php">O nama</a>
+  <a href="https://www.instagram.com/pavle.dj/">Moj instagram</a>
+  <a href="https://www.facebook.com/pavle.djurisic.777/">Moj facebook</a>
+  <a href="https://www.spotify.com/me-en/">Moj omiljeni sajt</a>
+  <a href="login.php">Login</a>
+  <a href="register.php">Register</a>
+  </body>
+  <div class="container">
+  <style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: yellow;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+  padding: 0px 0px;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: center;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 0px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
+<style>
+      body{
+          background-color: yellow;
+        }
+      </style>
+</head>
+<body>
+
+</body>
+</html>
+
+</style>
+</head>
+<body>
+<style>
+      body{
+          background-color: yellow;
+        }
+      </style>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1></h1>
+<br>
+<h1></h1>
+<br>
+<h1></h1>
+<html>
+<head>
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+</head>
+<body>
+
+<h1>KONTAKT BROJ 069317382 ili 067435217</h1>
+<h1>Naše Društvene Mreže</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.fa {
+  padding: 100px;
+  font-size: 10000px;
+  width: 100px;
+  text-align: center;
+  text-decoration: none;
+  margin: 100px 50px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-skype {
+  background: #00aff0;
+  color: white;
+}
+
+.fa-yahoo {
+  background: #430297;
+  color: white;
+}
+
+</style>
+</head>
+<body>
+
+<a href="https://www.facebook.com/pavle.djurisic.777/" class="fa fa-facebook"></a>
+<a href="https://twitter.com/?lang=en" class="fa fa-twitter"></a>
+<a href="https://www.youtube.com" class="fa fa-youtube"></a>
+<a href="https://www.instagram.com/pavle.dj/" class="fa fa-instagram"></a>
+<a href="https://www.skype.com/en/" class="fa fa-skype"></a>
+<a href="https://search.yahoo.com/web?guccounter=1" class="fa fa-yahoo"></a>
+      
+</body>
+</html> 
+
+</body>
+</html>
---
+++ omeni.php
@@ -0,0 +1,277 @@
+
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8" />
+  <title>Ivanovic co - Podgorica</title>
+  <meta name="description" content="." />
+	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <style>
+      body{
+          background-color: yellow;
+        }
+      </style>
+</head>
+
+<body style="font-family: Raleway, courier, serif;">
+<div class="header"> 
+</div>
+
+<div class="topnav">
+<a href="nikola.php">Home</a>
+     <a href="kontakt.php">Kontakt</a>
+    <a href="omeni.php">O nama</a>
+  <a href="https://www.instagram.com/pavle.dj/">Moj instagram</a>
+  <a href="https://www.facebook.com/pavle.djurisic.777/">Moj facebook</a>
+  <a href="https://www.spotify.com/me-en/">Moj omiljeni sajt</a>
+  <a href="login.php">Login</a>
+  <a href="register.php">Register</a>
+  </body>
+  <div class="container">
+  <style>
+* {box-sizing: border-box;}
+
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: yellow;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #2196F3;
+  color: white;
+}
+
+.topnav .search-container {
+  float: right;
+  padding: 0px 0px;
+}
+
+.topnav input[type=text] {
+  padding: 6px;
+  margin-top: 8px;
+  font-size: 17px;
+  border: none;
+}
+
+.topnav .search-container button {
+  float: center;
+  padding: 6px;
+  margin-top: 8px;
+  margin-right: 0px;
+  background: #ddd;
+  font-size: 17px;
+  border: none;
+  cursor: pointer;
+}
+
+.topnav .search-container button:hover {
+  background: #ccc;
+}
+
+@media screen and (max-width: 600px) {
+  .topnav .search-container {
+    float: none;
+  }
+  .topnav a, .topnav input[type=text], .topnav .search-container button {
+    float: none;
+    display: block;
+    text-align: left;
+    width: 100%;
+    margin: 0;
+    padding: 14px;
+  }
+  .topnav input[type=text] {
+    border: 1px solid #ccc;  
+  }
+}
+</style>
+<style>
+      body{
+          background-color: yellow;
+        }
+      </style>
+</head>
+<body>
+
+
+
+<div style="padding-left:0px">
+</div>
+
+</body>
+</html>
+
+</style>
+</head>
+<body>
+<style>
+      body{
+          background-color: yellow;
+        }
+      </style>
+<h1></h1>
+<br>
+<h1></h1>
+<br>
+<h1></h1>
+<html>
+<head>
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+</head>
+<body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1 style="text-align:left" >O nama</h1>
+<p><b>Mi smo firma koja vrši servis svih vozila, svih marki vozila. Tu smo za vas da vam pomognemo.
+  Firma se nalazi u Podgorici na Zabjelu. Radno vrijeme je od 09:00 do 17:00 časova. Nasa firma koja se bavi prodajom auto djelova jos od 1992 godine,u prodajnom programu 
+  nudimo djelove za VW-Audi,Opel,Ford, a od skoro i prosiren program za francuska vozila Peugeot,Renault i Citroen. Kod nas mozete naci i sirok izbor kozmetike i 
+  dodatne opreme za Vas auto (presvlake,patosnice,cerade,krovne nosace...) i to sve po najpovoljnijim cijenama,provjerite
+      </b></p>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      
+      <!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.fa {
+  padding: 20px;
+  font-size: 30px;
+  width: 100px;
+  text-align: center;
+  text-decoration: none;
+  margin: 5px 22px;
+}
+
+.fa:hover {
+    opacity: 0.7;
+}
+
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-youtube {
+  background: #bb0000;
+  color: white;
+}
+
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+
+.fa-snapchat-ghost {
+  background: black;
+  color: white;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+
+.fa-skype {
+  background: #00aff0;
+  color: white;
+}
+
+
+.fa-stumbleupon {
+  background: #eb4924;
+  color: white;
+}
+
+.fa-flickr {
+  background: #f40083;
+  color: white;
+}
+
+.fa-yahoo {
+  background: #430297;
+  color: white;
+}
+
+.fa-soundcloud {
+  background: #ff5500;
+  color: white;
+}
+
+.fa-reddit {
+  background: #ff5700;
+  color: white;
+}
+
+.fa-rss {
+  background: #ff6600;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-youtube"></a>
+<a href="#" class="fa fa-instagram"></a>
+
+<a href="#" class="fa fa-snapchat-ghost"></a>
+<a href="#" class="fa fa-skype"></a>
+<a href="#" class="fa fa-yahoo"></a>
+<a href="#" class="fa fa-reddit"></a>
+<a href="#" class="fa fa-rss"></a>
+      
+</body>
+</html>
